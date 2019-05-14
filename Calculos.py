2a4b54f69c1ff48900092a67211036ce04c4c527
Gustavo Nemeth <gustavo.nemeth@sattra.com.br>
2019-05-14 15:42:17 -0300

    Correcoes

--- Calculos.py
+++ Calculos.py
@@ -1,29 +1,58 @@
 from Peso import Pesos as Peso
-from dateutil.relativedelta import relativedelta
 
-data_lancamento = 1
-data_ultima_atualizacao = 1
-suporte_windows = 1
-suporte_linux = 1
-suporte_mac = 1
-acid = 1
-tamanho_maximo_banco = 1
-tamanho_maximo_tabela = 1
-tamanho_maximo_linha = 1
-tamanho_maximo_blob = 1
-hash = 1
-regex = 1
-queries_em_paralelo = 1
-cursor = 1
-proteção_força_bruta = 1
-auditoria = 1
 
-# 1 licenca paga / 0 open source
-get_peso_licenca(valor, recebido):
-if valor != recebido:
-    return valor * Peso.licenca
-return 0
+class Calculos:
 
-get_peso_data_lancamento(valor, recebido):
-diferenca = relativedelta(valor, recebido).years
-diferenca = diferenca if (diferenca > 0) else -diferenca
+    def validacao_generica_boolean(valor, recebido, peso):
+        if valor != recebido:
+            return valor * Peso.licenca
+        return 0
+
+    # tamanho_maximo_banco = 1
+    # tamanho_maximo_tabela = 1
+    # tamanho_maximo_linha = 1
+    # tamanho_maximo_blob = 1
+
+    # 1 licenca paga / 0 open source
+    def get_peso_licenca(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.licenca)
+
+    def get_peso_data_lancamento(valor, recebido):
+        diferenca = relativedelta(valor, recebido).years
+        diferenca = diferenca if (diferenca > 0) else -diferenca
+        return diferenca * Peso.data_lancamento
+
+    def get_peso_ultima_atualizacao(valor, recebido):
+        diferenca = relativedelta(valor, recebido).months
+        diferenca = diferenca if (diferenca > 0) else -diferenca
+        return diferenca * Peso.data_ultima_atualizacao
+
+    def get_peso_suporte_windows(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.suporte_windows)
+
+    def get_peso_suporte_linux(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.suporte_linux)
+
+    def get_peso_suporte_mac(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.suporte_mac)
+
+    def get_peso_acid(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.acid)
+
+    def get_peso_hash(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.hash)
+
+    def get_peso_regex(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.regex)
+
+    def get_peso_paralelismo(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.queries_em_paralelo)
+
+    def get_peso_cursor(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.cursor)
+
+    def get_peso_protecao_forca_bruta(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.proteção_força_bruta)
+
+    def get_peso_auditoria(valor, recebido):
+        return validacao_generica_boolean(valor, recebido, Peso.auditoria)
--- __init__.py
+++ __init__.py
@@ -1,14 +1,59 @@
+import datetime
+
 from Banco import Banco
+from Calculos import Calculos
 
+nome = "Referencia buscada"
+licenca = ""
+data_lancamento = datetime.strptime('2012-02-10', '%Y-%m-%d')
+data_ultima_atualizacao = datetime.strptime('2012-02-10', '%Y-%m-%d')
+suporte_windows = False
+suporte_linux = False
+suporte_mac = False
+acid = False
+hash = False
+regex = False
+queries_em_paralelo = False
+cursor = False
+proteção_força_bruta = False
+auditoria = False
 
-
-
+menor_valor = 9999999
+menor_banco = Banco
 arquivo = open("bancos.txt")
 bancos = list()
+
+max_lancamento = 0
+min_lancamento = 99999
+max_atualizacao = 0
+min_atualizacao = 99999
+
 for linha in arquivo:
     print(linha)
     attr = linha.split(";")
-    b = Banco(attr[0],attr[1],attr[2],attr[3],attr[4],attr[5],attr[6],attr[7],attr[8],attr[9],attr[10],attr[11],
-              attr[12],attr[13],attr[14],attr[15],attr[16],attr[17])
-    bancos.append(b)
+    b = Banco(attr[0], attr[1], attr[2], attr[3], attr[4], attr[5], attr[6], attr[7], attr[8], attr[9], attr[10],
+              attr[11],
+              attr[12], attr[13], attr[14], attr[15], attr[16], attr[17])
 
+for b in bancos:
+    bancos.append(b)
+    soma = 0
+    soma += Calculos.get_peso_licenca(licenca, b.licenca)
+    soma += Calculos.get_peso_data_lancamento(data_lancamento, b.data_lancamento)
+    soma += Calculos.get_peso_ultima_atualizacao(data_ultima_atualizacao, b.data_ultima_atualizacao)
+    soma += Calculos.get_peso_suporte_linux(linux, b.suporte_linux)
+    soma += Calculos.get_peso_suporte_mac(suporte_mac, b.suporte_mac)
+    soma += Calculos.get_peso_acid(acid, b.acid)
+    soma += Calculos.get_peso_hash(hash, b.hash)
+    soma += Calculos.get_peso_regex(regex, b.regex)
+    soma += Calculos.get_peso_paralelismo(queries_em_paralelo, b.queries_em_paralelo)
+    soma += Calculos.get_peso_cursor(cursor, b.cursor)
+    soma += Calculos.get_peso_protecao_forca_bruta(proteção_força_bruta, b.proteção_força_bruta)
+    soma += Calculos.get_peso_auditoria(auditoria, b.auditoria)
+
+    if soma < menor_valor:
+        menor_valor = soma
+        menor_banco = bancos
+
+print("Resposta =" + b.nome)
+print("Diferenca de " + menor_valor)
